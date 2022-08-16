8707b40974aeca0ec4a3eb17acd4d9a9d3a7e093
cf12 <11359669+cf12@users.noreply.github.com>
2022-08-16 18:28:26 -0400

    Add debian bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -12,4 +12,9 @@ elif [[ -f /etc/arch-release ]]; then
   sudo pacman --noconfirm -S vim neovim tmux fish kitty
 
   chsh -s /usr/bin/fish
+elif [[ -f /etc/lsb-release ]]; then
+  sudo apt-get update
+  sudo apt-get install -y vim neovim tmux fish kitty
+
+  chsh -s /usr/bin/fish
 fi
