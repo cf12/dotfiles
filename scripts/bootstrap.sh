013e959f30baf30322493d52c407ecd65511f661
cf12 <11359669+cf12@users.noreply.github.com>
2023-02-17 16:52:31 -0500

    update bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ "$(uname -s)" == "Darwin" ]; then
+  echo "[i] MacOS detected, bootstrapping..."
   if ! command -v brew &> /dev/null; then
+    echo "[i] Homebrew not found, downloading + installing..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 
@@ -9,17 +11,26 @@ if [ "$(uname -s)" == "Darwin" ]; then
   brew install font-hack
   brew install vim neovim tmux fish kitty pinentry-mac gnupg
 elif [[ -f /etc/arch-release ]]; then 
+  echo "[i] Arch Linux detected, bootstrapping..."
   sudo pacman --noconfirm -Sy
   sudo pacman --noconfirm -S ttf-hack
   sudo pacman --noconfirm -S vim neovim tmux fish kitty
 
   chsh -s /usr/bin/fish
 elif [[ -f /etc/lsb-release ]]; then
-  sudo apt-get update
-  sudo apt-get install -y fonts-hack-ttf
-  sudo apt-get install -y vim neovim tmux fish kitty
+  if grep -qi "microsoft" /proc/version; then
+    echo "[i] Linux (WSL) detected, bootstrapping..."
+    sudo apt-get update
+    sudo apt-get install -y vim neovim tmux fish
+  else
+    echo "[i] Linux (native) detected, bootstrapping..."
+    sudo apt-get update
+    sudo apt-get install -y fonts-hack-ttf
+    sudo apt-get install -y vim neovim tmux fish kitty
+  fi
 
   chsh -s /usr/bin/fish
 fi
 
+echo "[i] Running 'fisher update'"
 fish -c "fisher update"
